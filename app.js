b75e7539a19e2e6c9d89d1dc0d0bc9c6504cb72a
serycodes <serycodes@gmail.com>
2020-02-26 22:05:51 +0000

    Resize both emtoes and badges

--- app.js
+++ app.js
@@ -7,15 +7,19 @@ var canvases = [
   document.querySelector('#c28'),
   document.querySelector('#c56'),
   document.querySelector('#c112'),
+  document.querySelector('#c18'),
+  document.querySelector('#c36'),
+  document.querySelector('#c72'),
 ];
 
 sourceImg.addEventListener('load', () => {
   canvases.forEach((e) => {
     pica.resize(sourceImg, e, {
-      quality: 2,
+      quality: 3,
       alpha: true,
     });
   });
+  document.getElementById("downloadTrigger").disabled=false;
 });
 
 body.ondragover = (e) => {
@@ -25,5 +29,15 @@ body.ondragover = (e) => {
 body.ondrop = (e) => {
   e.preventDefault();
   console.log(e.dataTransfer.files);
+  window.filename = e.dataTransfer.files[0].name;
   sourceImg.src = URL.createObjectURL(e.dataTransfer.files[0]);
 };
+
+var downloadAll = () => {
+  canvases.forEach((c) => {
+      var link = document.createElement('a');
+      link.download = c.width + "-" + window.filename;
+      link.href = c.toDataURL()
+      link.click();
+  })
+}
