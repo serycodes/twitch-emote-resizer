32c5036ed01e6f6243d2126c06e666b512e08e39
serycodes <serycodes@gmail.com>
2020-02-29 23:33:39 +0000

    Add cropping and zip download

--- app.js
+++ app.js
@@ -1,8 +1,10 @@
 var pica = pica();
+var cropper, filename, filetype;
 var URL = window.URL || window.webkitURL;
 
 var body = document.querySelector('body');
 var sourceImg = document.querySelector('#source');
+var croppedImg = document.querySelector('#cropped');
 var canvases = [
   document.querySelector('#c28'),
   document.querySelector('#c56'),
@@ -13,8 +15,23 @@ var canvases = [
 ];
 
 sourceImg.addEventListener('load', () => {
+  cropper = new Cropper(sourceImg, {
+    aspectRatio: 1/1,
+    viewMode: 1,
+    autoCrop: true,
+    autoCropArea: 1
+  })
+  document.getElementById("hideCropper").hidden=false;
+});
+
+
+sourceImg.addEventListener('crop', (event) => {
+  croppedImg.src = cropper.getCroppedCanvas().toDataURL(filetype)
+});
+
+croppedImg.addEventListener('load', () => {
   canvases.forEach((e) => {
-    pica.resize(sourceImg, e, {
+    pica.resize(croppedImg, e, {
       quality: 3,
       alpha: true,
     });
@@ -29,15 +46,34 @@ body.ondragover = (e) => {
 body.ondrop = (e) => {
   e.preventDefault();
   console.log(e.dataTransfer.files);
-  window.filename = e.dataTransfer.files[0].name;
+  filename = e.dataTransfer.files[0].name;
+  filetype = e.dataTransfer.files[0].type;
+  cropper != null ? cropper.destroy() : null;
+  canvases.forEach((c) => {
+    c.getContext("2d").clearRect(0,0,c.width,c.height);
+  })
   sourceImg.src = URL.createObjectURL(e.dataTransfer.files[0]);
 };
 
-var downloadAll = () => {
-  canvases.forEach((c) => {
-      var link = document.createElement('a');
-      link.download = c.width + "-" + window.filename;
-      link.href = c.toDataURL()
-      link.click();
+var downloadAll = async () => {
+  var zip = new JSZip();
+  var promises = canvases.map(async (c, id) => {
+    var blobData = await new Promise(resolve => c.toBlob(resolve));
+    zip.file(c.width + "-" + filename, blobData)
   })
+
+  await Promise.all(promises);
+
+  zip.generateAsync({type:"blob"})
+  .then(function(content) {
+      // see FileSaver.js
+      saveAs(content, filename + ".zip");
+  });
+
 }
+
+var hideCropper = () => {
+  document.getElementById("hideCropper").hidden=true;
+  cropper.destroy();
+}
+
